431814c184f15378c6466f3ed08133f1aad0c32a
yenairong <1039288191@qq.com>
2018-10-15 21:08:45 +0800

    re

--- cc/src/main/java/com/ly/webservice/StockCode.java
+++
@@ -1,9 +0,0 @@
-package com.ly.webservice;
-
-import javax.jws.WebParam;
-import javax.jws.WebService;
-
-@WebService
-public interface StockCode {
-    public String getStock(@WebParam(name="stockCode")String stockCode);
-}
